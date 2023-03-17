4c63b92c846b3a510f7572fae7be05cdee89e5ba
Bebetta Motondo <claramotondo2@yahoo.fr>
2023-03-17 11:09:25 -0500

    Classes 1

---
+++ Classes/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Classes
+{
+
+	public class Car
+	{
+		public Car ()
+		{
+
+		}
+
+		public Car (string make, string model, int year)
+		{
+			Make = make;
+			Model = model;
+			Year = year;
+		}
+		
+		public string Make { get; set; }
+		public string Model {get; set;}
+	    public int Year { get; set; } 
+
+	}
+}
+
